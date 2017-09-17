da6de60e62496ed800e1ab62c122c74794d09112
penguoir <games@marash.net>
2017-09-17 17:55:45 +0100

    Remove about page

--- server.js
+++ server.js
@@ -19,10 +19,6 @@ app.get('/', (req, res) => {
   res.sendFile( path.join(__dirname, 'index.html') );
 });
 
-app.get('/about', (req, res) => {
-  res.sendFile( path.join(__dirname, 'about.html') );
-})
-
 io.on('connection', socket => {
   socket.on('move', e => {
     arduino.move(e);
